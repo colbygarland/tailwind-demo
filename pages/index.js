59cf345df0d3057a3c1df58d3d0255b00362d938
Colby Garland <colbygarland@gmail.com>
2020-11-16 09:17:40 -0800

    Removed unused imports from index

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,7 @@
-import PageHeader from '../components/PageHeader'
 import Customization from '../components/Customization'
 import Apply from '../components/Apply'
 import Responsive from '../components/Responsive'
 import Utility from '../components/Utility'
-import Content from '../components/Content'
-import Container from '../components/Container'
-import Title from '../components/Title'
 import Links from '../components/Links'
 
 export default function Home() {
